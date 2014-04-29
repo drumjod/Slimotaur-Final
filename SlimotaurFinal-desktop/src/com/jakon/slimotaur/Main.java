e1978aa36b1b8268d76fbfe5b585ee30abe4740d
drumjod <drumjod@gmail.com>
2014-04-28 18:22:36 -0600

    Added author comment to Main.java

--- SlimotaurFinal-desktop/src/com/jakon/slimotaur/Main.java
+++ SlimotaurFinal-desktop/src/com/jakon/slimotaur/Main.java
@@ -1,5 +1,6 @@
 package com.jakon.slimotaur;
-
+//@author Jon Wareham
+//@author Jake Jones
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
